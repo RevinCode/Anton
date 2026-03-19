e29d1009beba1c13a320ad52d10a26899bc3e337
revin <nicerevin@gmail.com>
2026-03-19 15:56:57 +0100

    Create towerbuilder.js

---
+++ towerbuilder.js
@@ -0,0 +1,15 @@
+javascript:(function(){
+    if(window.log && log.log){
+        var old = log.log;
+        log.log = function(d){
+            if(d.event === "finishGameTowerBuilder"){
+                d.score = 45; 
+                d.remainingTries = 2;
+                d.isDebug = false;
+                console.log("Tower Builder Hacked: 45 Floors");
+            }
+            return old.apply(this, arguments);
+        };
+        alert("Tower Builder Active!");
+    }
+})();
