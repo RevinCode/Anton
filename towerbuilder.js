f5f1cd1902c68236b8fbe9c8476d280b159f9b81
revin <nicerevin@gmail.com>
2026-03-19 15:57:19 +0100

    Update towerbuilder.js

--- towerbuilder.js
+++ towerbuilder.js
@@ -3,7 +3,7 @@ javascript:(function(){
         var old = log.log;
         log.log = function(d){
             if(d.event === "finishGameTowerBuilder"){
-                d.score = 45; 
+                d.score = 1767; 
                 d.remainingTries = 2;
                 d.isDebug = false;
                 console.log("Tower Builder Hacked: 45 Floors");
